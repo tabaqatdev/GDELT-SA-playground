218e2fc3bb69b88c1005afb13dd4b5fc8ccd0819
yharby <me@youssefharby.com>
2026-01-29 00:29:02 +0200

    chore: disable console.log in production

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  // Optional: Disable others if strict
+  // console.info = () => {};
+  // console.debug = () => {};
+}
+
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
